66af0fe541acddbfdcbbfb0cbe1d9f4007ec7892
oak86 <email@omarkhafagy.com>
2013-09-01 09:08:43 -0400

    add more loop output, create instance of class to test index

--- html_generator.rb
+++ html_generator.rb
@@ -9,26 +9,39 @@ class HtmlGenerator
 	## Anything that's not related to the output should not be in this method.
 	
 	## First, an html header needs to be printed
-	html_header =
+	## html_header <--- This needs to become a method for later, but for now...
 		p 	"<html>"
 		p 	"<head>" 
 		p 	"<title>LCBO Connoisseur</title>"
 		p   "</head>"
 		p 	"</body>"
 	## Then I need to give this content a title. We'll call it Index of Products
-	p "<h1> Index of Products </h1> "
+		p "<h1> Index of Products </h1> "
 	## After that, I need to bring in the parsed index information
-	parsed_index
+	parsed_index = pull_data
 
 	## Then I gotta loop through each item in the array and output the information.
 
 		parsed_index.each do |drink|
 			#Put the name of the drink as an html subhead and separate div tag
-			#Put the information of the drink
+			p "<div class = 'drink'>"
+			p "<h2></h2>"
+			#Put the information of the drink... we'll just do name and origin for now.
+			p "<p>#{name}</p>"
+			p "<p>#{tags}</p>"
+			p "</div>"
 	    end
 
 	## Then I gotta add an html footer
 	html_footer
+	end
+
+	def pull_data
+		raw_data = open("http://lcboapi.com/products").read
+		parsed_data = JSON.parse(raw_data)
+
+		return parsed_data
+
 	end
 
 	def show(product_id)
@@ -46,23 +59,7 @@ class HtmlGenerator
 	def html_footer
 		##Outline the basic html footer information
 	end
-
-	def pull_data
-		#puts "HtmlGenerator: show"
-		#raw_response = open("http://lcboapi.com/products").read
-
-		#Parse JSON-formatted text int oa Ruby Hash
-		#parsed_response = JSON.parse(raw_response)
-
-		#Return the actual result data from the response, ignoring metadata
-		#products = parsed_response["result"]
-
-		#puts products
-
-
-		raw_data = open("http://lcboapi.com/products").read
-		parsed_data = JSON.parse(raw_data)
-
-		return parsed_data
-	end
 end
+
+generator = HtmlGenerator.new
+generator.index
